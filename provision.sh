c5ec91ed514dcd14371ea94e5124f3ddce29abaa
McKay Davis <mckay.davis@gmail.com>
2015-06-26 12:24:00 -1000

    Install CRIU 1.5.2 package for `runc checkpoint` functionality

--- provision.sh
+++ provision.sh
@@ -2,7 +2,13 @@
 
 sudo aptitude update
 sudo aptitude upgrade -y
-sudo aptitude install golang -y
+sudo aptitude install python-ipaddr python-protobuf libprotobuf-c1 golang -y
+
+cd /tmp
+
+wget http://http.us.debian.org/debian/pool/main/c/criu/criu_1.5.2-1_amd64.deb
+
+sudo dpkg -i criu_1.5.2-1_amd64.deb
 
 mkdir /home/vagrant/gocode
 
@@ -14,6 +20,7 @@ EOF
 export GOPATH=/home/vagrant/gocode
 export PATH=$PATH:$GOPATH/bin
 
+cd $HOME
 go get github.com/tools/godep
 go get -d github.com/opencontainers/runc
 cd $GOPATH/src/github.com/opencontainers/runc/
